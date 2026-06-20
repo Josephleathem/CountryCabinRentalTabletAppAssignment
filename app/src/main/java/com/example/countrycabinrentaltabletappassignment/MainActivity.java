8abecfff2d78b18beeb8a7e44c3acc1a1434b6f9
Josephleathem <Jleat2620@gmail.com>
2026-06-20 01:53:06 -0400

    added Image and image description switch based on which radio button is clicked. adjusted the layout/view so the app fits on the screen.

--- app/src/main/java/com/example/countrycabinrentaltabletappassignment/MainActivity.java
+++ app/src/main/java/com/example/countrycabinrentaltabletappassignment/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.countrycabinrentaltabletappassignment;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -33,8 +34,24 @@ public class MainActivity extends AppCompatActivity {
 
         RadioGroup cabinRadioGroup = findViewById(R.id.cabinRadioGroup);
         Button selectDateButton = findViewById(R.id.selectDateButton);
+        ImageView cabinImage = findViewById(R.id.cabinImage);
+        TextView cabinDescription = findViewById(R.id.cabinDescription);
         TextView cabinValue = findViewById(R.id.cabinValue);
         TextView datesValue = findViewById(R.id.datesValue);
+
+        cabinRadioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            if (checkedId == R.id.radioCabinOne) {
+                cabinImage.setImageResource(R.drawable.mountain_view_cabin);
+                cabinImage.setContentDescription(getString(R.string.cabin_one_name));
+                cabinDescription.setText(R.string.cabin_one_description);
+            } else if (checkedId == R.id.radioCabinTwo) {
+                cabinImage.setImageResource(R.drawable.cabin_in_the_woods);
+                cabinImage.setContentDescription(getString(R.string.cabin_two_name));
+                cabinDescription.setText(R.string.cabin_two_description);
+            }
+        });
+
+        cabinRadioGroup.check(R.id.radioCabinOne);
 
         selectDateButton.setOnClickListener(v -> {
             int checkedId = cabinRadioGroup.getCheckedRadioButtonId();
